5236e939c4317d142414a2b038edee86ea5b2855
wpellas <william.pellas@hotmail.com>
2023-02-11 08:36:15 +0100

    1.1.2

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -3,7 +3,7 @@ import "../Components.css";
 
 
 function Merits() {
-    const skills = ["HTML5 & CSS3", "JavaScript", "React.js", "Tailwind", "Node.js", "PostgreSQL & MySQL", "Illustrator & SVG", "Agile Project Methodology", "Scrum & Trello", "Project Management", "Photography", "Livestreaming", "Adobe Suite", "3D Graphics", "Copyright"];
+    const skills = ["HTML5 & CSS3", "JavaScript & TypeScript", "React.js", "Tailwind", "Node.js & Express.js", "PostgreSQL & MySQL", "Illustrator & SVG", "Agile Project Methodology", "Scrum & Trello", "Project Management", "Photography", "Livestreaming", "Adobe Suite", "3D Graphics", "Copyright"];
     let key = 0;
     return (
         <div className="section w-[100%]">
