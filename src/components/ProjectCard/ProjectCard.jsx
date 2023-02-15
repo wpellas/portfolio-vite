d6bb9c52a6ffd9465a371959073fe8917e7de7e4
wpellas <william.pellas@hotmail.com>
2023-02-15 08:38:55 +0100

    1.3.2

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -4,7 +4,7 @@ import "../Components.css";
 const ProjectCard = ({ id, projectName, type, imgUrl, projectUrl, faIcons }) => (
         <div className="stealth hiddenSlide project">
             <p className="text-xs md:text-base lg:text-xl xl:text-2xl pb-1 pt-3 text-amber-300 font-mono tracking-widest">{projectName.toUpperCase()}</p>
-            <div key={id} className="w-28 sm:w-40 md:w-48 lg:w-64 xl:w-72 h-28 sm:h-40 md:h-48 lg:h-64 xl:h-72 border-2 border-solid border-cyan-300 rounded-lg shadow shadow-cyan-500 bg-cover hover:border-amber-300 hover:shadow-amber-500">
+            <div key={id} className="w-[7.5rem] sm:w-40 md:w-48 lg:w-64 xl:w-72 h-[7.5rem] sm:h-40 md:h-48 lg:h-64 xl:h-72 border-2 border-solid border-cyan-300 rounded-lg shadow shadow-cyan-500 bg-cover hover:border-amber-300 hover:shadow-amber-500">
                 <a href={projectUrl} target="_blank">
                     <div className="w-[100%] h-[100%] bg-cover rounded-md" style={{backgroundImage: `url(${imgUrl})`}}>
                         <div className="text-left absolute bottom-0 mb-1">
