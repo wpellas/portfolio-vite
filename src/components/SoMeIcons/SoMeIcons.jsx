ee4ee591b2edf6db4789c9becca3b9d8375b7cae
wpellas <william.pellas@hotmail.com>
2023-06-14 08:25:02 +0200

    0.6.1

--- src/components/SoMeIcons/SoMeIcons.jsx
+++ src/components/SoMeIcons/SoMeIcons.jsx
@@ -9,7 +9,9 @@ const SoMeIcons = () => (
                         <li key={item.id}>
                             <a
                             name={item.name} 
-                            className={`${item.styles} text-amber-300 text-lg sm:text-3xl border border-solid border-cyan-300 rounded-full w-10 h-10 sm:w-16 sm:h-16 flex flex-wrap items-center justify-center hover:transition-all hover:text-cyan-300 hover:border-amber-300 hover:scale-105`} href={item.url} target="_blank">
+                            className={`${item.styles} text-amber-300 text-lg sm:text-3xl border border-solid border-cyan-300 rounded-full w-10 h-10 sm:w-16 sm:h-16 flex flex-wrap items-center justify-center hover:transition-all hover:text-cyan-300 hover:border-amber-300 hover:scale-105`}
+                            href={item.url} 
+                            target="_blank">
                             </a>
                         </li>
                     )
